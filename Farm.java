3e31f3395f7e953d7cf4eb500a10c1c3f722a2bf
blytheoconnor <32681854+blytheoconnor@users.noreply.github.com>
2018-11-25 13:53:17 -0400

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,10 +1,14 @@
-class Farm 
-{     
-   private Animal[] aBunchofAnimals;
+class Farm {     
+   private Animal[] aBunchOfAnimals;
    public Farm() {
-     aBunchofAnimals = new Animal[3];
-     aBunchofAnimals[0] = new Cow("cow", "moo");
-     aBunchofAnimals
-     aBunchofAnimals
+     aBunchOfAnimals = new Animal[3];
+     aBunchOfAnimals[0] = new Cow("cow ", "moo");
+     aBunchOfAnimals[1] = new Chick("chick ", "cluck");
+     aBunchOfAnimals[2] = new Pig("pig ", "oink");
+   }
+   public void animalSounds() {
+      for (int nI=0; nI < aBunchOfAnimals.length; nI++) {
+       System.out.println(aBunchOfAnimals[nI].getType() + "goes " + aBunchOfAnimals[nI].getSound());
+     }
    }
 }
